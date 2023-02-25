87a30e6556ef9c3c6ac555fcdcc303342d177911
Reto Stamm <reto@retostamm.com>
2023-02-25 10:52:56 -0800

    Skipping commandline test on windows

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,7 +1,11 @@
 from subprocess import Popen, PIPE, STDOUT
+import os
 
 
 def test_commandline_installed():
+    if os.name == "nt":
+        return  # The following does not work as a test on windows
+
     p = Popen(["phagetrix"], stdout=PIPE, stderr=STDOUT)
     out, _ = p.communicate()
     niceout = out.decode("utf-8")
