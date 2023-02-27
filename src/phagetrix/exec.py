35a84b46c68d94908a5ed57d5bf7503fddd50387
Reto Stamm <reto@retostamm.com>
2023-02-27 14:18:41 -0800

    bump version 0.1.12 -> 0.2.0

--- src/phagetrix/exec.py
+++ src/phagetrix/exec.py
@@ -161,7 +161,7 @@ def main():
     parser.description = "PhageTrix"
     parser.long_description = "Generate degenerate primers for phage display libraries"
     parser.epilog = epilog
-    parser.version = "0.1.12"
+    parser.version = "0.2.0"
     parser.add_argument(
         "input", type=argparse.FileType("r"), metavar="INPUT_FILE", help="Input file"
     )
