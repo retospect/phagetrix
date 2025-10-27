a1a5dd2959fc31e3d4b44ec4bfd88be62462201c
GitHub Action <action@github.com>
2025-10-27 22:13:59 +0000

    bump version to 1.0.3

--- src/phagetrix/__init__.py
+++ src/phagetrix/__init__.py
@@ -33,7 +33,7 @@ from .core import DegenerateCodonGenerator
 from .output import OutputFormatter
 from .parser import InputParser
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 # Public API - what users see with "from phagetrix import *"
 __all__ = [
